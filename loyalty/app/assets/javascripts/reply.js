7bf9225169ad71ba3a7f0273bd988e4315292433
yurimann <yuri.manahan@gmail.com>
2017-05-23 14:20:06 -0400

    Replace let with var

--- loyalty/app/assets/javascripts/reply.js
+++ loyalty/app/assets/javascripts/reply.js
@@ -6,14 +6,11 @@ $(document).on("ready", function(){
     $('html, body').animate({
         scrollTop: $("#nav").offset().top
     });
-    let message = $(this).siblings(".text").html()
-    // $(".show").toggleClass("form-modal");
-    let parentId = $(this).children().val();
-    // let temp = $(this).parent();
-    // let tabValue = parseInt(temp.css('margin-left'), 10);
-    // let addWidth = ($(window).width() * .05);
+    var message = $(this).siblings(".text").html()
+    var parentId = $(this).children().val();
+    
     $("#master").attr("value", parentId);
-    // $("#tab").attr("value", tabValue + addWidth);
+
     $(".new-message").html("Reply to " + message)
     $(".insert").html("<div class='cancel'>" + "Cancel" + "</div>")
   });
